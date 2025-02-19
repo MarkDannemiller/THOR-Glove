259be102f3406995ae8e0d4dfd9dbfd08d5f05c8
Charlie Scheel <109446478+CharlieScheel@users.noreply.github.com>
2025-02-19 17:28:55 -0600

    Initial Script

---
+++ lab4_2025219/lab4_2025219.ino
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+// calibration constants
+#define VCC 3.3         // 3.3V Logic
+#define PDR 5100        // pull down resistor
+// test/get these values
+#define OPEN_THRESH 1   // threshold force to open finger
+#define CLOSE_THRESH 5  // threshold force to close finger
+#define A 1.0           // calibration constant for force
+#define B 0.5           // calibration constant for force
+#define ANGLE_INC 5     // increment for servo angle
+
+uint16_t adc_read;  // reading from ADC pin
+float adc_voltage;  // adc_read converted to voltage value
+float resistance;   // resistance calcuation
+float force;        // force calculation
+int fsrPin;         // targeted ADC input pin
+int servoPin;       // targeted servo pin
+float servoAngle;   // angle of targeted servo
+
+void setup() {
+    Serial.begin(115200);
+    analogReadResolution(12);  // 12-bit ADC (0-4095)
+
+    fsrPin = A0;
+    servoPin = 0;
+}
+
+void loop() {
+    // combine after testing
+    adc_read = analogRead(fsrPin);  
+    adc_voltage = (adc_read/4095.0)*VCC;
+    resistance = (VCC-adc_voltage)*PDR/adc_voltage;
+
+    // force equation F=A*(R^-B)
+    force = A*pow(resistance, -B); 
+
+    Serial.printf("ADC: %u, Voltage: %.2fV, Resistance: %.2fΩ, Force: %.2fN\n",
+                  adc_read, adc_voltage, resistance, force);
+
+    // adjust servo angle if force is outside threshold
+    servo.attach(servoPin);    // alternate: (pin,pwm_0,pwm_180)
+    servoAngle = servo.read();
+    if (force < CLOSE_THRESH) { servo.write(servoAngle+ANGLE_INC); } 
+    else if (force > OPEN_THRESH) { servo.write(servoAngle-ANGLE_INC); }
+    servo.detach();
+
+    delay(100);
+}
