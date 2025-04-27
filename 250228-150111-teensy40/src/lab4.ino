5e82c2309b172fe5d5be2d95678faddc4d44578a
Charlie Scheel <109446478+CharlieScheel@users.noreply.github.com>
2025-04-27 17:44:18 -0500

    pin assignments & buttons

--- 250228-150111-teensy40/src/lab4.ino
+++ 250228-150111-teensy40/src/lab4.ino
@@ -5,12 +5,14 @@
 
 // calibration constants
 #define VCC 3.3         // 3.3V Logic
-#define PCA_NUM_PORTS 16
 #define SERVO_MAX_ANGLE 270
 #define SERVO_MIN_ANGLE 0
 #define SERVO_MIN_PULSE 500
 #define SERVO_MAX_PULSE 2500
-#define NUM_PORTS 2
+#define SERVO_DEFAULT   90
+#define NUM_FINGERS 5
+#define NUM_BUTTONS 3
+#define NUM_LEDS    2
 #define MAX_CURRENT 2300 // 2.3 A to mA
 
 // test for these values
@@ -19,19 +21,32 @@
 
 uint16_t adc_read;    // reading from ADC pin
 float adc_voltage;    // adc_read converted to voltage value
-float  adc_current;  // adc_read converted to current value
+float  adc_current;   // adc_read converted to current value
 float servo_angle;    // angle of targeted servo
+Servo servoSelect;    // analog servos run at ~60 Hz updates
 int counter;
-Servo servoSelect;  // Analog servos run at ~60 Hz updates
 
 
-//Parameters
-int FSR_PINS [NUM_PORTS] = {A0};
-int CURRENT_PINS [NUM_PORTS] = {1};
-int SERVO_PINS [NUM_PORTS] = {0};
+// Pin Assignments
 
+// pinky, ring, middle, index, thumb
+int FSR_PINS [NUM_FINGERS] =      {14,16,18,20,22};
+int CURRENT_PINS [NUM_FINGERS] =  {15,17,19,21,23};
+int SERVO_PINS [NUM_FINGERS] =    {2, 3, 4, 5, 7};
+
+int BUTTON_PINS [NUM_BUTTONS] = {8,9,10}; // max, min, start
+int LED_PINS [NUM_LEDS] = {12,13};
+
+// Map servo angle to pulse width
 double angleToMicroseconds(int angle) {
   return map(angle, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE, SERVO_MIN_PULSE, SERVO_MAX_PULSE);
+}
+
+// Write pwm to servo
+void writeToServo(int counter, float servo_angle) {
+  servoSelect.attach(SERVO_PINS[counter]);
+  servoSelect.writeMicroseconds(angleToMicroseconds((int)servo_angle+3)); // +3 to increase tension
+  servoSelect.detach();
 }
 
 void setup() {
@@ -44,6 +59,7 @@ void setup() {
 }
 
 void loop() {
+
   // calculate voltage from resistance reading
   adc_read = analogRead(FSR_PINS[counter]);
   adc_voltage = (adc_read / 4095.0) * VCC;  // 12-bit ADC (0-4095)
@@ -51,21 +67,27 @@ void loop() {
   // get current reading
   adc_current = ACS712(CURRENT_PINS[counter], VCC, 4095, 100).mA_AC(); //  ACS712 20A uses 100 mV per A
 
-  if (adc_voltage > FSR_THRESH || adc_current < CURRENT_THRESH) {
-    servo_angle = (adc_voltage / 2.75) * SERVO_MAX_ANGLE; // Map voltage (0-3V) to servo angle (0° to upper limit)
+  // button settings
+  if(BUTTON_PINS[0]) {        writeToServo(counter, SERVO_MAX_ANGLE);
+  } else if(BUTTON_PINS[1]) { writeToServo(counter, SERVO_MIN_ANGLE);
+  } else if(BUTTON_PINS[2]) { writeToServo(counter, SERVO_DEFAULT);
+
+  // general control logic
+  } else if (adc_voltage > FSR_THRESH && adc_current < CURRENT_THRESH) {
+
+    // Map voltage (0-3V) to servo angle (0° to upper limit)
+    servo_angle = (adc_voltage / 2.75) * SERVO_MAX_ANGLE; 
 
     // cap servo angle within bounds
     if (servo_angle < SERVO_MIN_ANGLE) servo_angle = SERVO_MIN_ANGLE;
     if (servo_angle > SERVO_MAX_ANGLE) servo_angle = SERVO_MAX_ANGLE;
 
     // write pwm to servo
-    servoSelect.attach(SERVO_PINS[counter]);
-    servoSelect.writeMicroseconds(angleToMicroseconds((int)servo_angle+3)); // +3 to increase tension
-    servoSelect.detach();
+    writeToServo(counter, servo_angle);
   }
 
   // increment counter
-  counter++;
+  counter = (counter+1)%NUM_FINGERS;
 
   delay(200);
 }
